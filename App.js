725d1312c37b8b4a63a19e25c149188835ac7503
Stacy Burris <stacy1burris@gmail.com>
2021-02-02 00:07:46 -0800

    data in console

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import oldTimey from './data/oldTimey.json';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// export default function App() {
+//   const [name, setName] = useState('');
+//   console.log('My data', oldTimey)
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +15,49 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    fontWeight: 'bold',
+    color: '#ff0000',
+  }
 });
+
+console.log('My data', oldTimey)
+class OldQuotes extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: ''
+    }
+  }
+}
+// https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B1000%2C788%5D&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F24%2F2017%2F07%2Fgettyimages-563963027-2000.jpg
+getSaying(); {
+  oldTimey[Math.floor(Math.random() * oldTimey.length)];
+}
+
+render(); {
+  const question = this.getSaying();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Whats the Cat's Meow{name}</Text>
+      <Button
+        onPress={() => setName('Bob')}
+        title="click me"
+      ></Button>
+      <Text
+        style={question.quote}
+      ></Text>
+      <Button
+        onPress={question.meaning}
+        title="display meaning"
+      ></Button>
+
+      <StatusBar style="auto" />
+    </View >
+
+  )
+}
+
+export default OldQuotes;
